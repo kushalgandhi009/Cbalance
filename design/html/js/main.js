80ff4a4d9f21e01e156f5d571b02a733f45bcffe
kushalgandhi009 <kushalgandhi009@gmail.com>
2012-11-29 12:09:53 +0530

    Effects added to banner - knob

--- design/html/js/main.js
+++ design/html/js/main.js
@@ -1,16 +1,38 @@
-var banners = $('#banner div').size();
+var $  = jQuery;
 var i = 1;
+var banner = $('#banner div');
+var banners = $(banner).size();
+var knob = $('input#knob');
+var knob_val = $(knob).attr('value');
+
+setInterval(function(){
+    if(i!=6){
+        knob_val = parseInt(knob_val) + 1;
+        $('.dial').val(knob_val).trigger('change');
+    }else{
+        knob_val  = 0;
+        $('.dial').val(0).trigger('change');
+        $('canvas').parent('div').hide("fast");
+    }
+},200);
+
 setInterval(function(){
     if(i<banners)
     {
-        $('#banner div').hide("slow");
+        $(banner).hide("slow");
         $('#banner div:eq('+i+')').show("slow");
     }
     else
     {
         i = 0;
-        $('#banner div').hide("slow");
+        $(banner).hide("slow");
+        $('.dial').val(0).trigger('change');
+        $('canvas').parent('div').show("fast");
         $('#banner div:eq('+i+')').show("slow");
     }
     i = i+1;
 },5000);
+$(function() {
+    $(".dial").knob();
+    $('canvas').parent('div').addClass('center');
+});
